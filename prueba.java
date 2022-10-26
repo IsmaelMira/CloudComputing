b2db74218ebb3937049988a813f1b0ab2307ba73
Luis Felipe QUesada Ramírez <luisfelipedeveloper@gmail.com>
2022-10-26 19:46:47 +0200

    errors solucionados

--- prueba.java
+++ prueba.java
@@ -1,5 +1,7 @@
-public static void main(String[] args) {
-    print("otra prueba más");
-    print("no sabemos nada");
-    print("esto es una prueba");
+class prueba {
+    public static void main(String[] args) {
+        System.out.print("otra prueba más");
+        System.out.print("no sabemos nada");
+        System.out.print("esto es una prueba");
+    }
 }
