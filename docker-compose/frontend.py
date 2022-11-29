240b16afa85a6f2bf6082b2f02e37c2269099858
Ismael Mira <ismamira20@gmail.com>
2022-11-29 13:53:59 +0100

    Node webapp

--- docker-compose/frontend.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import request
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  id = request.args.get("id")
-  return "Mensaje insertado con id: {0}".format(id)
